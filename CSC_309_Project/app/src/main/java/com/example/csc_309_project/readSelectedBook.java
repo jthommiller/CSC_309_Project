8efe242209f76f4c318e0fdaab99127aae91239f
James Miller <jthommiller@yahoo.com>
2019-11-14 14:49:45 -0500

    basic design for app

---
+++ CSC_309_Project/app/src/main/java/com/example/csc_309_project/readSelectedBook.java
@@ -0,0 +1,14 @@
+package com.example.csc_309_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class readSelectedBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_selected_book);
+    }
+}
