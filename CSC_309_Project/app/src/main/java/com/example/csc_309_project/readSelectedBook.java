d3ea0f56db2e3bf02f08e226893b4408ac7c0b8f
James Miller <jthommiller@yahoo.com>
2019-11-19 12:43:02 -0500

    Design Update
    Updated the design and transitions so that function code can be added.

--- CSC_309_Project/app/src/main/java/com/example/csc_309_project/readSelectedBook.java
+++ CSC_309_Project/app/src/main/java/com/example/csc_309_project/readSelectedBook.java
@@ -2,7 +2,13 @@ package com.example.csc_309_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class readSelectedBook extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class readSelectedBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_selected_book);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
+
+        ImageButton returnToLibrary = findViewById(R.id.returnToUserBookMenu);
+        returnToLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), userBookMenu.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
--- CSC_309_Project/app/src/main/java/com/example/csc_309_project/userBookMenu.java
+++ CSC_309_Project/app/src/main/java/com/example/csc_309_project/userBookMenu.java
@@ -9,17 +9,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class userBookMenu extends AppCompatActivity {
+
+    //"F7F9F9, AFA2FF, 3C3744, C8C6AF, 808D8E"
+
+    final static String usersLibraryTitle = "Your Library";
+    final static String addToLibraryTitle = "Download Books To Your Library";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_book_menu);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        final TextView tv = findViewById(R.id.mainMenuTitle);
+        tv.setText(usersLibraryTitle);
+
+        final TabLayout tl = findViewById(R.id.navigationTabBar);
+        tl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 0){
+                    tv.setText(usersLibraryTitle);
+                }
+                else{
+                    tv.setText(addToLibraryTitle);
+                }
+                //tl.setSelectedTabIndicatorColor(Color.rgb(60, 55, 68));
+                tl.setBackgroundColor(Color.rgb(60, 55, 68));
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         Button test = findViewById(R.id.test);
         test.setOnClickListener(new View.OnClickListener(){
